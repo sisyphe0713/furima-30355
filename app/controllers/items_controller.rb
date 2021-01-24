ba2e71270f829c6e632e3856d9f9f33e71160291
sisyphe0713 <sisyphe0713@gmail.com>
2021-01-24 21:25:58 +0900

    商品データの保存
    -createアクションのルーティング設定
    -createアクションをコントローラーに定義
    -バリデーションを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,20 @@
 class ItemsController < ApplicationController
+  
   def index
   end
 
   def new
     @item = Item.new
   end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :text, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :ship_day_id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :price
+    validates :text
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :ship_day_id
+  end
 end
