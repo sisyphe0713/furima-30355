c6140f3d3889d3669e719a5db84a135e83e5aad2
sisyphe0713 <sisyphe0713@gmail.com>
2021-02-01 03:39:41 +0900

    orderテーブルとaddressテーブルに同時にデータ保存可能に

---
+++ app/models/order_address.rb
@@ -0,0 +1,19 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :post_code, :prefecture_id, :city, :address_number, :building, :telephone, :user_id, :item_id
+  
+  with_options presence: true do
+    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Inclue hyphen(-)" }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :city
+    validates :address_number
+    validates :telephone, format: { with: /\A\d{,11}\z/, message: "is less than 11 degit without hyphen(-)" }
+  end
+
+  def save
+    user = User.find(user_id)
+    item = Item.find(item_id)
+    order = Order.create(user_id: user.id, item_id: item.id)
+    Address.create(order_id: order.id, post_code: post_code, prefecture_id: prefecture_id, city: city, address_number: address_number, building: building, telephone: telephone)
+  end
+end
